7f73568ed4877cb8113ac8607178f0348268f64d
Crowdbotics <team@crowdbotics.com>
2024-03-14 17:46:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "national-air-cargo-47059",
+  url: "https://national-air-cargo-47059.botics.co",
+  api: "https://national-air-cargo-47059.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
